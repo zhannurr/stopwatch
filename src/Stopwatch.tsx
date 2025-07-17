17f261a4e705716228e18153a2c7f6ba61822d41
zhaksyb <you@example.com>
2025-07-17 14:18:26 +0500

    timer is working

--- src/Stopwatch.tsx
+++ src/Stopwatch.tsx
@@ -5,26 +5,52 @@ import { TouchableOpacity, View, Text } from "react-native";
 function Stopwatch(){
     const [isRunning, setIsRunning] = useState(false);
     const [elapsedTime, setElapsedTime] = useState(0);
-    const intervalIdRef = useRef(null);
+    const intervalIdRef = useRef<NodeJS.Timeout | null>(null);
     const startTimeRef = useRef(0);
 
     useEffect(() => {
+        if(isRunning){
+            intervalIdRef.current = setInterval(() =>{
+                setElapsedTime(Date.now()- startTimeRef.current);
+            },10);
+        }
 
-
+        return () => {
+            if (intervalIdRef.current) {
+                clearInterval(intervalIdRef.current);
+            }
+        }
     },[isRunning]);
 
     function start(){
+        setIsRunning(true);
+        startTimeRef.current = Date.now() - elapsedTime;
 
     }
 
     function stop() {
+        setIsRunning(false);
         
     }
     function reset() {
+        setElapsedTime(0);
+        setIsRunning(false);
         
     }
     function formatTime(){
-        return '00:00:00';
+
+        let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+        let minutes = Math.floor(elapsedTime / (1000 * 60)%60);
+        let seconds = Math.floor(elapsedTime / (1000) %60);
+        let miliseconds = Math.floor(elapsedTime % 1000)/10;
+
+        let newminutes = String(minutes).padStart(2, "0");
+        let newseconds = String(seconds).padStart(2, "0");
+        let newmiliseconds = String(miliseconds)
+        newmiliseconds = newmiliseconds.slice(0, newmiliseconds.length-2);
+        newmiliseconds = newmiliseconds.padStart(2, "0");
+
+        return `${newminutes}: ${newseconds} : ${newmiliseconds}`;
 
     }
     return (
